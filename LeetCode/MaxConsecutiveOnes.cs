d3c75a71da2342dbfd77aa510c6dceb7d58b0814
Kristoffer <bluekris29@hotmail.com>
2023-03-20 17:15:02 +0000

    More challenges completed

---
+++ LeetCode/MaxConsecutiveOnes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode
+{
+    public static class MaxConsecutiveOnes
+    {
+        //MaxConsecutiveOnes
+        //Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+        //Example 1:
+
+        //Input: nums = [1,1,0,1,1,1]
+        //        Output: 3
+        //Explanation: The first two digits or the last three digits are consecutive 1s.The maximum number of consecutive 1s is 3.
+        //Example 2:
+
+        //Input: nums = [1,0,1,1,0,1]
+        //        Output: 2
+ 
+
+        //Constraints:
+
+        //1 <= nums.length <= 105
+        //nums[i] is either 0 or 1.
+
+
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0;
+            int count =0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (max < count)
+                    {
+                        max = count;
+                    }
+                    count = 0;
+                }
+            }
+            if (max < count)
+            {
+                max = count;
+            }
+            return max;
+        }
+    }
+}
---
+++ LeetCode/SquaresOfSortedArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode
+{
+    public class SquaresOfSortedArray
+    {
+        //SquaresOfSortedArray
+        //Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+        //Example 1:
+
+        //Input: nums = [-4, -1, 0, 3, 10]
+        //Output: [0,1,9,16,100]
+        //Explanation: After squaring, the array becomes[16, 1, 0, 9, 100].
+        //After sorting, it becomes [0,1,9,16,100].
+        //Example 2:
+
+        //Input: nums = [-7, -3, 2, 3, 11]
+        //Output: [4,9,9,49,121]
+
+        //Constraints:
+
+        //1 <= nums.length <= 104
+        //-104 <= nums[i] <= 104
+        //nums is sorted in non-decreasing order.
+
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] squared = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] * nums[i];
+            }
+            squared = nums.OrderBy(x => x).ToArray();
+            return squared;
+        }
+    }
+}
